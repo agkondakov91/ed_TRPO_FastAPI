8a71e0a1e729b213881883a037657599f57c6ca6
Alexander Kondakov <agkondakov91@gmail.com>
2026-05-26 00:53:25 +0300

    fix: add pr 3L

--- app/routers/lessons.py
+++ app/routers/lessons.py
@@ -1,20 +1,19 @@
 from fastapi import APIRouter, HTTPException, status
 
 from app.data import lessons
+from app.schemas.lesson import LessonCreateSchema, LessonReadSchema, LessonUpdateSchema
 
 router = APIRouter(prefix='/lessons', tags=['Lessons'])
 
 
-# GET    /lessons/
-@router.get('/')
-def get_lessons(active: bool | None = None):
-    if active is None:
+@router.get('/', response_model=list[LessonReadSchema])
+def get_lessons(course_id: int | None = None):
+    if course_id is None:
         return lessons
-    return [lesson for lesson in lessons if lesson['is_active'] == active]
+    return [lesson for lesson in lessons if lesson['course_id'] == course_id]
 
 
-# GET    /lessons/{lesson_id}
-@router.get('/{lesson_id}')
+@router.get('/{lesson_id}', response_model=LessonReadSchema)
 def get_lesson(lesson_id: int):
     for lesson in lessons:
         if lesson['id'] == lesson_id:
@@ -24,37 +23,31 @@ def get_lesson(lesson_id: int):
     )
 
 
-# POST   /lessons/
 @router.post('/', status_code=status.HTTP_201_CREATED)
-def create_lesson(course_id: int, title: str):
+def create_lesson(lesson_schema: LessonCreateSchema):
     new_lesson = {
         'id': len(lessons) + 1,
-        'course_id': course_id,
-        'title': title,
-        'is_active': True,
+        'course_id': lesson_schema.course_id,
+        'title': lesson_schema.title,
+        'is_active': lesson_schema.is_active,
     }
     lessons.append(new_lesson)
     return new_lesson
 
 
-# PATCH  /lessons/{lesson_id}
-@router.patch('/{lesson_id}')
-def partially_update_lesson(
-    lesson_id: int, title: str | None = None, is_active: bool | None = None
-):
+@router.patch('/{lesson_id}', response_model=LessonReadSchema)
+def partially_update_lesson(lesson_id: int, lesson_schema: LessonUpdateSchema):
     for lesson in lessons:
         if lesson['id'] == lesson_id:
-            if title is not None:
-                lesson['title'] = title
-            if is_active is not None:
-                lesson['is_active'] = is_active
+            update_data = lesson_schema.model_dump(exclude_unset=True)
+            for key, value in update_data.items():
+                lesson[key] = value
             return lesson
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND, detail='Lesson not found'
     )
 
 
-# DELETE /lessons/{lesson_id}
 @router.delete('/{lesson_id}', status_code=status.HTTP_204_NO_CONTENT)
 def delete_lesson(lesson_id: int):
     for lesson in lessons:
