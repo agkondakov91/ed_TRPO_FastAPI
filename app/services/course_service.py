e707bc4d3bf8e8888ceaa76caf4ff1bf197c5471
Alexander Kondakov <agkondakov91@gmail.com>
2026-06-04 23:08:05 +0300

    fix: 6.5 services

--- app/services/course_service.py
+++ app/services/course_service.py
@@ -2,7 +2,11 @@ from sqlalchemy.orm import Session
 
 from app.models.course import Course
 from app.repositories import course_repository
-from app.schemas.course import CoursePostSchema
+from app.schemas.course import (
+    CoursePostSchema,
+    CourseReplaceSchema,
+    CourseUpdateSchema,
+)
 
 
 def get_courses(
@@ -31,15 +35,19 @@ def create_course(db: Session, course_data: CoursePostSchema) -> Course:
     return course_repository.create_course(db, new_course)
 
 
-# def replace_course(course_id: int, course_data: CourseReplaceSchema) -> dict | None:
-#     new_course_data = course_data.model_dump()
-#     return course_repository.replace_course(course_id, new_course_data)
-#
-#
-# def update_course(course_id: int, course_data: CourseUpdateSchema) -> dict | None:
-#     update_data = course_data.model_dump(exclude_unset=True)
-#     return course_repository.update_course(course_id, update_data)
-#
-#
-# def delete_course(course_id: int) -> bool:
-#     return course_repository.delete_course(course_id)
+def replace_course(
+    db: Session, course_id: int, course_data: CourseReplaceSchema
+) -> Course | None:
+    new_course_data = course_data.model_dump()
+    return course_repository.update_course_fields(db, course_id, new_course_data)
+
+
+def update_course(
+    db: Session, course_id: int, course_data: CourseUpdateSchema
+) -> Course | None:
+    update_data = course_data.model_dump(exclude_unset=True)
+    return course_repository.update_course_fields(db, course_id, update_data)
+
+
+def delete_course(db: Session, course_id: int) -> bool:
+    return course_repository.delete_course(db, course_id)
